d455ff59d35b55d1baabc04b08c2392b30cf0628
berkleylim <berkleylim16@gmail.com>
2021-10-10 23:28:02 +0900

    introduction controller upload

---
+++ back/src/main/java/author/hyun/sik/lim/web/json/IntroductionController.java
@@ -0,0 +1,15 @@
+package author.hyun.sik.lim.web.json;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class IntroductionController {
+    
+    @GetMapping("/")
+    void getIntroduction() {
+        
+    }
+}
