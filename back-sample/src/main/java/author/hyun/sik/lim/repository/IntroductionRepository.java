15d2696222ca94baa00264c643fe9f73a42dd45c
berkleylim <berkleylim16@gmail.com>
2021-10-11 23:02:20 +0900

    spring init

---
+++ back-sample/src/main/java/author/hyun/sik/lim/repository/IntroductionRepository.java
@@ -0,0 +1,9 @@
+package author.hyun.sik.lim.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import author.hyun.sik.lim.domain.Introduction;
+
+public interface IntroductionRepository extends JpaRepository<Introduction, Integer> {
+
+}
