77b5678ba0b3a5e16ab3a05b898b530e00f62cfe
berkleylim <berkleylim16@gmail.com>
2021-10-11 23:02:26 +0900

    spring init

---
+++ profile-back/src/main/java/profile/back/ProfileBackApplication.java
@@ -0,0 +1,13 @@
+package profile.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileBackApplication.class, args);
+	}
+
+}
---
+++ profile-back/src/test/java/profile/back/ProfileBackApplicationTests.java
@@ -0,0 +1,13 @@
+package profile.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
