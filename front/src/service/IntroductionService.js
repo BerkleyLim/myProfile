bfd181420d4e41cae807bb9de14e85b91fb9d6f2
berkleylim <berkleylim16@gmail.com>
2021-11-13 22:47:53 +0900

    profile upload

--- front/src/service/IntroductionService.js
+++ front/src/service/IntroductionService.js
@@ -4,7 +4,7 @@ const API_BASE_URI = "http://localhost:8080/api/introduction"
 
 class IntroductionService {
     getIntroduction() {
-        return axios.get(API_BASE_URI);
+        return axios.get(API_BASE_URI + "/");
     }
 
 }
