c0bcff1a748eb245248938eae2ee1e7c42ad1138
berkleylim <berkleylim16@gmail.com>
2021-11-16 19:59:11 +0900

    spring boot refactoring

--- profile-back/src/main/java/profile/back/configuration/DBConfiguration.java
+++ profile-back/src/main/java/profile/back/configuration/DBConfiguration.java
@@ -20,37 +20,36 @@ import com.zaxxer.hikari.HikariDataSource;
 //      https://jforj.tistory.com/91
 //      https://blog.jiniworld.me/55
 //      https://jogeum.net/10
+//      https://congsong.tistory.com/51
 @Configuration
 @PropertySource("classpath:/application.properties")
-//@PropertySource("classpath:/aws.properties")
-//@MapperScan(basePackages={"profile.back.domain.mapper"}, sqlSessionFactoryRef="sqlSessionFactory", sqlSessionTemplateRef="sqlSessionTemplate")
+@MapperScan(basePackages={"profile.back.mapper"}, sqlSessionFactoryRef="sqlSessionFactory", sqlSessionTemplateRef="sqlSessionTemplate")
 public class DBConfiguration {
-
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.hikari")
-    public HikariConfig hikariConfig() {
-        return new HikariConfig();
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        return new HikariDataSource(hikariConfig());
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
-        factoryBean.setDataSource(dataSource());
-        factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/*Mapper.xml"));
-        return factoryBean.getObject();
-    }
-
-    @Bean
-    public SqlSessionTemplate sqlSession() throws Exception {
-        return new SqlSessionTemplate(sqlSessionFactory());
-    }
+//    @Autowired
+//    private ApplicationContext applicationContext;
+//
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+//    public HikariConfig hikariConfig() {
+//        return new HikariConfig();
+//    }
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        return new HikariDataSource(hikariConfig());
+//    }
+//
+//    @Bean
+//    public SqlSessionFactory sqlSessionFactory() throws Exception {
+//        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+//        factoryBean.setDataSource(dataSource());
+//        factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/*Mapper.xml"));
+//        return factoryBean.getObject();
+//    }
+//
+//    @Bean
+//    public SqlSessionTemplate sqlSession() throws Exception {
+//        return new SqlSessionTemplate(sqlSessionFactory());
+//    }
 
 }
--- profile-back/src/main/java/profile/back/domain/Introduction.java
+++ profile-back/src/main/java/profile/back/domain/Introduction.java
@@ -1,5 +1,7 @@
 package profile.back.domain;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,19 +12,21 @@ import javax.persistence.Table;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+@Getter @Setter
 @Entity
-@Setter
-@Getter
-@AllArgsConstructor
 @Table(name="introduction")
 @DynamicInsert
 @DynamicUpdate
-public class Introduction {
+public class Introduction implements Serializable {
     
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int no;
