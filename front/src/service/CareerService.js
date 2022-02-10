e9f584c6b2f7ed40e8138dccaf0cbb29473ca9eb
berkleylim <berkleylim16@gmail.com>
2022-02-10 23:09:03 +0900

    profile upload

--- front/src/service/CareerService.js
+++ front/src/service/CareerService.js
@@ -4,11 +4,11 @@ const API_BASE_URI = "http://localhost:8080/api/career"
 
 class IntroductionService {
     getCareer() {
-        return axios.get(API_BASE_URI);
+        return axios.get(API_BASE_URI + "career");
     }
 
     getSkill() {
-        return axios.get(API_BASE_URI);
+        return axios.get(API_BASE_URI + "skill");
     }
 
 }
