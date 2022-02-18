9d9d43a56c84eb674a7eb467dc1de77a807fc21b
berkleylim <berkleylim16@gmail.com>
2022-02-18 16:51:20 +0900

    profile upload

--- front/src/App.js
+++ front/src/App.js
@@ -8,6 +8,26 @@ import ProjectComponent from './components/project/ProjectComponent'
 import AppComponent from './components/AppComponent'
 import TogetherComponent from './components/together/TogetherComponent'
 import TogetherDetailComponent from './components/together/TogetherDetailComponent'
+// import LoginService from './service/LoginService';
+
+// 참조 : https://velog.io/@yaytomato/%ED%94%84%EB%A1%A0%ED%8A%B8%EC%97%90%EC%84%9C-%EC%95%88%EC%A0%84%ED%95%98%EA%B2%8C-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EC%B2%98%EB%A6%AC%ED%95%98%EA%B8%B0
+// onLogin = (email, password) => {
+// 	const data = {
+// 		email,
+// 		password,
+// 	};
+// 	axios.post('/login', data).then(response => {
+// 		const { accessToken } = response.data;
+
+// 		// API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+// 		axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+
+// 		// accessToken을 localStorage, cookie 등에 저장하지 않는다!
+
+// 	}).catch(error => {
+// 		// ... 에러 처리
+// 	});
+// };
 
 function App() {
   return (
--- front/src/service/LoginService.js
+++ front/src/service/LoginService.js
@@ -4,7 +4,17 @@ const API_BASE_URI =  "http://localhost:8080/"
 
 class LoginService {
     login(data){
-        return axios.request(API_BASE_URI + "login", data);
+        return axios.request(API_BASE_URI + "login", data).then(response => {
+ 		const { accessToken } = response.data;
+    
+        // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+     		axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
+    
+     		// accessToken을 localStorage, cookie 등에 저장하지 않는다!
+    
+     	}).catch(error => {
+     		// ... 에러 처리
+     	});
     }
 
     logout(){
