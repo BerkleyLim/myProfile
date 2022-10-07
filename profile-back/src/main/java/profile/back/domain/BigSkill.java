acb1ed679add71095faf6ba2bceada912416e282
berkleylim <berkleylim16@gmail.com>
2022-10-08 00:40:32 +0900

    profile upload - mariadb에서 대소문자 구분하는 버전에서 임의로 테이블명 변경

--- profile-back/src/main/java/profile/back/domain/BigSkill.java
+++ profile-back/src/main/java/profile/back/domain/BigSkill.java
@@ -24,7 +24,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 //mysql과 oracle에서는 table은 필요 없지만 mariadb에서는 대소문자 구별하므로 설정
-@Table(name="bigSkill")
+@Table(name="bigskill")
 @DynamicInsert
 @DynamicUpdate
 public class BigSkill implements Serializable {
