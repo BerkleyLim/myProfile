32ca870dc5cb9047bec89098641de7d6be6c4545
berkleylim <berkleylim16@gmail.com>
2022-10-07 18:51:54 +0900

    profile upload - setting

--- profile-back/src/main/java/profile/back/configuration/WebConfig.java
+++ profile-back/src/main/java/profile/back/configuration/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://35.90.180.8:3000");
     }
 }
