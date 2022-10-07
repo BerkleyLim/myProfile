8ae65c307ca0f944e4c58857d29212ff76205934
berkleylim <berkleylim16@gmail.com>
2022-10-07 23:27:58 +0900

    profile upload

---
+++ profile-back/src/main/resources/static/precache-manifest.baf756eb44bbe529a8e16a27c3e2ce56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "46c588344ea6cd1b7c90",
+    "url": "/static/css/main.4f9da2a3.chunk.css"
+  },
+  {
+    "revision": "46c588344ea6cd1b7c90",
+    "url": "/static/js/main.61dc63b1.chunk.js"
+  },
+  {
+    "revision": "17038e1ea44717336948",
+    "url": "/static/js/runtime~main.0e2fc12b.js"
+  },
+  {
+    "revision": "bc1439f3ecaff5738a0e",
+    "url": "/static/css/2.8abb23c2.chunk.css"
+  },
+  {
+    "revision": "bc1439f3ecaff5738a0e",
+    "url": "/static/js/2.100a752a.chunk.js"
+  },
+  {
+    "revision": "00f62158032c23924eb7",
+    "url": "/static/js/3.80abd554.chunk.js"
+  },
+  {
+    "revision": "b92b85c9952e83d190fea5d2b03388d5",
+    "url": "/static/media/Interior.b92b85c9.jpg"
+  },
+  {
+    "revision": "2ca29cc1c57b0139f8cee2b41b831d82",
+    "url": "/static/media/MusicStyle.2ca29cc1.png"
+  },
+  {
+    "revision": "eb806564be5070f6c6e1247dca69a5a1",
+    "url": "/static/media/Chanel.eb806564.jpg"
+  },
+  {
+    "revision": "b6a69728fbc15ce96a3751b19f13a48f",
+    "url": "/index.html"
+  }
+];
