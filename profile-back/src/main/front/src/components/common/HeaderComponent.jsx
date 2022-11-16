c8538d5a1d1a09cf9bb44a17a3631cce3ce6e1b2
임현식 <limhyunsik@imhyeonsig-ui-MacBookPro.local>
2022-11-16 09:51:46 +0900

    prettier 방식으로 소스 코드를 깔끔히 정리해주고 헤더 변경

--- profile-back/src/main/front/src/components/common/HeaderComponent.jsx
+++ profile-back/src/main/front/src/components/common/HeaderComponent.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPrint } from '@fortawesome/free-solid-svg-icons'
+import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPrint } from "@fortawesome/free-solid-svg-icons";
 import { Link, useNavigate } from "react-router-dom";
 // import { StaticRouter as NavLink } from "react-router-dom/server";
 // import LoginService from '../../service/LoginService'
@@ -21,7 +21,7 @@ export default function HeaderComponent(props) {
 
   // info_print() {
   //   let initBody = document.body;
-  //   let hiddenBtn = document.querySelector('.print-button'); 
+  //   let hiddenBtn = document.querySelector('.print-button');
   //   let hiddenHeader = document.querySelector('#header');
   //   let hiddenNavbar = document.querySelector('.navbar-device');
   //   let hiddenClearfix = document.querySelector('.clearfix');
@@ -45,19 +45,19 @@ export default function HeaderComponent(props) {
 
   const movePage = (manu) => {
     navigate(manu);
-  }
+  };
 
   const handleLoginClick = (data) => {
     // window.alert(data);
     // let session = LoginService.login(data);
     // alert(session);
     props.onLogin(data);
-  }
+  };
 
   const handleLogoutClick = () => {
     props.onLogout();
     document.location.reload();
-  }
+  };
 
   const searchMenu = (keyword) => {
     // console.log(this.state.loginStatus)
@@ -74,64 +74,85 @@ export default function HeaderComponent(props) {
       handleLogoutClick();
       // window.alert(this.state.loginStatus);
     } else {
-      // 검색  
-
+      // 검색
     }
-    // window.alert(sessionStorage.length);  
+    // window.alert(sessionStorage.length);
     // window.alert(this.state.isLogin);
-
-  }
-
+  };
 
   return (
-    <div className='header'>
-      <div className='navbar navbar-expand-lg'>
-        <div className='nav container-fluid'>
-          <a className="nav nav-link" aria-current="page" href="http://35.90.180.8:8080">My.Dev </a>
+    <div className="header">
+      <div className="navbar navbar-expand-lg">
+        <div className="nav container-fluid">
+          <a
+            className="nav nav-link"
+            aria-current="page"
+            href="http://localhost:8080/"
+          >
+            My.Dev{" "}
+          </a>
           {/* <div className="collapse navbar-collapse" id="navbarColor01"> */}
           {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button> */}
+          <li className="nav-item">
+            <button
+              className="nav-link active"
+              onClick={() => movePage("/introduction")}
+            >
+              소개
+            </button>
+          </li>
+          <li class="nav-item">
+            <button className="nav-link" onClick={() => movePage("/career")}>
+              이력 및 기술
+            </button>
+          </li>
+          <li class="nav-item">
+            <button className="nav-link" onClick={() => movePage("/project")}>
+              프로젝트
+            </button>
+          </li>
+          <li class="nav-item">
+            <Link to="/together" style={{ textDecoration: "none" }}>
+              <button className="nav-link">파트너모집</button>
+            </Link>
+          </li>
+          <form className="d-flex">
+            <input
+              id="search-keyword"
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-light"
+              onClick={() =>
+                searchMenu(document.getElementById("search-keyword").value)
+              }
+            >
+              Search
+            </button>
+          </form>
+          <li class="nav-item">
+            <button className="nav-link" onClick="info_print()">
+              <FontAwesomeIcon icon={faPrint} />
+            </button>
+          </li>
+          {props.isLogin ? (
+            // Main 컴포넌트 호출 시 isLogin 이라는 props 값을 전달
             <li className="nav-item">
-              <button className="nav-link active" onClick={() => movePage('/introduction')}>
-                소개
+              <button className="btn" onClick={handleLogoutClick}>
+                로그아웃
               </button>
             </li>
-            <li class="nav-item">
-              <button className="nav-link" onClick={() => movePage('/career')}>
-                이력 및 기술
-              </button>
-            </li>
-            <li class="nav-item">
-              <button className="nav-link" onClick={() => movePage('/project')}>
-                프로젝트
-              </button>
-            </li>
-            {/* <li class="nav-item">
-                              <Link to="/together" style={{ textDecoration: 'none' }}>
-                                <button className="nav-link">
-                                  파트너모집
-                                </button>
-                              </Link>
-                            </li> */}
-            <form className="d-flex">
-              <input id="search-keyword" className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-light" onClick={() => searchMenu(document.getElementById("search-keyword").value)}>Search</button>
-            </form>
-            <li class="nav-item">
-              <button className="nav-link" onClick="info_print()">
-                <FontAwesomeIcon icon={faPrint} />
-              </button>
-            </li>
-            {props.isLogin ?
-              // Main 컴포넌트 호출 시 isLogin 이라는 props 값을 전달
-              <li className="nav-item"><button className="btn" onClick={handleLogoutClick}>로그아웃</button></li> :
-              <></>}
+          ) : (
+            <></>
+          )}
           {/* </div> */}
         </div>
       </div>
     </div>
-  )
+  );
 }
-
-
