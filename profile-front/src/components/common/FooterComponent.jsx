c1fd9164eebbf3fc624f0b8f9f4055218293ce72
berkleylim <berkleylim16@gmail.com>
2023-03-15 01:45:55 +0900

    warning 해결

--- profile-front/src/components/common/FooterComponent.jsx
+++ profile-front/src/components/common/FooterComponent.jsx
@@ -4,13 +4,13 @@ export default function FooterComponent() {
     return (
         <div className="footer">
             <div className="ft-content">
-                <p className="ft-service">
+                <div className="ft-service">
                     <div>Github : <a href="https://github.com/BerkleyLim">https://github.com/BerkleyLim</a></div>
                     <div>E-Mail : <a href="mailto:berkleylim16@gmail.com">berkleylim16@gmail.com</a></div>
                     <div>Kakao : </div>
                     <div>Follow : facebook, instagram</div>
-                </p>
-                <span className="ft-copy">Copyright (c) 2022 MyDev., Ltd, All Rights Reserved</span>
+                </div>
+                <span className="ft-copy">Copyright (c) 2023 MyDev., Ltd, All Rights Reserved</span>
             </div>
         </div>  
     )  
