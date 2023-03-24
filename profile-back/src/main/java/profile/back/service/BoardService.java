08b58e800b1fc1c0703588c2e870c3cefe8b90cb
berkleylim <berkleylim16@gmail.com>
2023-03-25 00:17:45 +0900

    콘솔 출력 제거

--- profile-back/src/main/java/profile/back/service/BoardService.java
+++ profile-back/src/main/java/profile/back/service/BoardService.java
@@ -119,8 +119,8 @@ public class BoardService {
     // 상세 보기 클릭시 조회 수 올리기 (차후 IP별로 클릭으로 변경 예정)
     public ResponseEntity<Board> viewClick(Board clickBoard) {
         Board board = boardRepository.findByBno(clickBoard.getBno());
-        System.out.println(board);
-        System.out.println(clickBoard.getViewNumber());
+        // System.out.println(board);
+        // System.out.println(clickBoard.getViewNumber());
         board.setViewNumber(clickBoard.getViewNumber());
         Board newBoard = boardRepository.save(board);
         return ResponseEntity.ok(newBoard);
