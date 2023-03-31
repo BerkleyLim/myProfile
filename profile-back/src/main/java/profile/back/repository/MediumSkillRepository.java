b355a9f21df367e95d02f50d982a70c3c698cc1e
berkleylim <berkleylim16@gmail.com>
2023-03-31 15:13:11 +0900

    사용 안하는 메소드 제거

--- profile-back/src/main/java/profile/back/repository/MediumSkillRepository.java
+++ profile-back/src/main/java/profile/back/repository/MediumSkillRepository.java
@@ -2,7 +2,6 @@ package profile.back.repository;
 
 import java.util.List;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import profile.back.domain.MediumSkill;
@@ -10,4 +9,5 @@ import profile.back.domain.MediumSkill;
 public interface MediumSkillRepository extends JpaRepository<MediumSkill, Long> {
   // @Query("select * from mediumskill m Orderby m.bigskill.no asc")
   // List<MediumSkill> findAllOrderby();
+
 }
