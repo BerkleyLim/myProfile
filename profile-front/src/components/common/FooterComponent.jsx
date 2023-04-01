7d973f1be8f4dc9b18cd8a433e24d2d046a7a294
berkleylim <berkleylim16@gmail.com>
2023-04-01 23:39:36 +0900

    feat : 제작자 변경

--- profile-front/src/components/common/FooterComponent.jsx
+++ profile-front/src/components/common/FooterComponent.jsx
@@ -5,14 +5,25 @@ export default function FooterComponent() {
         <div className="footer">
             <div className="ft-content">
                 <div className="ft-service">
-                    <div>Github : <a href="https://github.com/BerkleyLim">https://github.com/BerkleyLim</a></div>
-                    <div>E-Mail : <a href="mailto:berkleylim16@gmail.com">berkleylim16@gmail.com</a></div>
+                    <div>Github : <a href="https://github.com/BerkleyLim">https://github.com/glidong.hong</a></div>
+                    <div>E-Mail : <a href="mailto:berkleylim16@gmail.com">gildong.hong@gmail.com</a></div>
                     <div>Kakao : </div>
                     <div>Follow : facebook, instagram</div>
                 </div>
                 <span className="ft-copy">Copyright (c) 2023 MyDev., Ltd, All Rights Reserved</span>
             </div>
         </div>  
+        // <div className="footer">
+        //     <div className="ft-content">
+        //         <div className="ft-service">
+        //             <div>Github : <a href="https://github.com/BerkleyLim">https://github.com/BerkleyLim</a></div>
+        //             <div>E-Mail : <a href="mailto:berkleylim16@gmail.com">berkleylim16@gmail.com</a></div>
+        //             <div>Kakao : </div>
+        //             <div>Follow : facebook, instagram</div>
+        //         </div>
+        //         <span className="ft-copy">Copyright (c) 2023 MyDev., Ltd, All Rights Reserved</span>
+        //     </div>
+        // </div>  
     )  
 }
 
