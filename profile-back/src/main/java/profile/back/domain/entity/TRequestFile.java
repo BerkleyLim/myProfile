745cbc5d42ff7884b0c8d591e859b29ac828dbce
berkleylim <berkleylim16@gmail.com>
2023-05-22 22:10:35 +0900

    refactor: 의뢰 데이터 DB 구성 수정 후 변환

---
+++ profile-back/src/main/java/profile/back/domain/entity/TRequestFile.java
@@ -0,0 +1,42 @@
+package profile.back.domain.entity;
+
+import java.io.Serializable;
+// import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+// mysql과 oracle에서는 table은 필요 없지만 mariadb에서는 대소문자 구별하므로 설정
+@Table(name = "trequestfile")
+@DynamicInsert
+@DynamicUpdate
+public class TRequestFile implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long rfno;
+
+  @Column(columnDefinition = "integer")
+  String rno;
+  @Column(columnDefinition = "integer")
+  String gupidx;
+  @Column(columnDefinition = "blob")
+  String file;
+
+}
---
+++ profile-back/src/main/java/profile/back/domain/vo/request/RequestDataForm.java
@@ -0,0 +1,8 @@
+package profile.back.domain.vo.request;
+
+import lombok.Data;
+
+@Data
+public class RequestDataForm {
+  Long rno;
+}
