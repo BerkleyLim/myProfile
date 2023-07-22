627f43fd13daee5a4e9c89f49667eba87d2119a0
berkleylim <berkleylim16@gmail.com>
2023-07-22 14:42:44 +0900

    fix: Admin - mobile용 UI 업데이트
    관리자 페이지 - 모바일 편의성 개편

--- profile-front/src/components/admin/index.js
+++ profile-front/src/components/admin/index.js
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./admin.module.scss"
 import Menu from "./menu/index"
+import MMenu from "./menu/mIndex"
 import { Route, Routes } from 'react-router-dom'
 
 import AdminRequest from './request'
 import AdminBoard from './board'
+import { Col, Row } from 'reactstrap'
 
 const Admin = () => {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const resizeListener = () => {
+      setInnerWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", resizeListener);
+  }, [innerWidth]);
+
   return (
+    <> {
+      innerWidth <= 750 ?
+      <div className={`${styles?.mAdminContainer}`}>
+        <Row className={`${styles?.mAdminHeader}`}>
+          <Col className={`${styles?.mAdminHeaderLogo}`}>
+            CleanWare.Inc
+          </Col>
+          <Col className={`${styles?.mAdminHeaderMenu}`}>
+            <MMenu />
+          </Col>
+          <div className={`${styles?.mAdminContents}`}>
+            <Routes>
+                <Route path="/admin/board" element={<AdminBoard />} />
+                <Route path="/admin/request" element={<AdminRequest />} />
+            </Routes>
+          </div>
+        </Row>
+      </div>
+      :
     <div className={`${styles?.adminContainer}`}>
       <div className={`${styles?.adminMenu}`}>
         <Menu />
@@ -19,6 +49,9 @@ const Admin = () => {
         </Routes>
       </div>
     </div>
+
+    }
+    </>
   )
 }
 
---
+++ profile-front/src/components/admin/menu/mIndex.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState } from "react";
+import { ListUl } from "react-bootstrap-icons";
+import { useNavigate } from "react-router-dom";
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
+
+const MAdminMenu = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+  const navigate = useNavigate();
+  const returnMain = () => {
+    window.location.href = process.env.REACT_APP_HOME_URL + "/";
+  };
+  return (
+    <div>
+      <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+        <DropdownToggle caret>
+          <ListUl />
+        </DropdownToggle>
+        <DropdownMenu container="body">
+          <DropdownItem onClick={() => navigate("/admin/profile")}>
+            프로필 관리
+          </DropdownItem>
+          <DropdownItem onClick={() => navigate("/admin/board")}>
+            게시판/공지
+          </DropdownItem>
+          <DropdownItem onClick={() => navigate("/admin/request")}>
+            의뢰목록
+          </DropdownItem>
+          <DropdownItem onClick={() => navigate("/admin/request")}>
+            대쉬보드
+          </DropdownItem>
+          <DropdownItem onClick={() => returnMain()}>
+            돌아가기
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  );
+};
+
+export default MAdminMenu;
