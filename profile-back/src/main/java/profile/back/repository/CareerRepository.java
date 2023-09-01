7b15797f92b0689d8f66c39237840f64748cdf12
berkleylim <berkleylim16@gmail.com>
2023-09-01 23:24:20 +0900

    remove: 안쓰는 파일 제거 - 백엔드 부분 전부 제거

--- profile-back/src/main/java/profile/back/repository/CareerRepository.java
+++
@@ -1,9 +0,0 @@
-package profile.back.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import profile.back.domain.entity.career.Career;
-
-public interface CareerRepository extends JpaRepository<Career, Long> {
-
-}
--- profile-back/src/main/java/profile/back/repository/SmallSkillRepository.java
+++
@@ -1,9 +0,0 @@
-package profile.back.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import profile.back.domain.entity.career.SmallSkill;
-
-public interface SmallSkillRepository extends JpaRepository<SmallSkill, Long> {
-
-}
