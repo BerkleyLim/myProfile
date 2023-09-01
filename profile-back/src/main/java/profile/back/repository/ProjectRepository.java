a0653e2c3695eabda2e558c968b2aef755a3da94
berkleylim <berkleylim16@gmail.com>
2023-09-01 23:31:11 +0900

    remove: 필요없는 파일 제거 - 백엔드 부분 제거

--- profile-back/src/main/java/profile/back/repository/ProjectRepository.java
+++
@@ -1,9 +0,0 @@
-package profile.back.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import profile.back.domain.entity.project.Project;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-
-}
--- profile-back/src/main/java/profile/back/repository/ProjectV2Repository.java
+++
@@ -1,9 +0,0 @@
-package profile.back.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import profile.back.domain.entity.project.ProjectV2;
-
-public interface ProjectV2Repository extends JpaRepository<ProjectV2, Long> {
-
-}
