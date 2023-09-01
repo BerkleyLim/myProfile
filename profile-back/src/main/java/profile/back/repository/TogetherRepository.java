8f0c7cac2bb82729b73bb33daf01492dc46539df
berkleylim <berkleylim16@gmail.com>
2023-09-01 23:22:46 +0900

    remove: 안쓰는 파일 제거 - 함께해요 관련 백엔드 제거

--- profile-back/src/main/java/profile/back/repository/TogetherRepository.java
+++
@@ -1,9 +0,0 @@
-package profile.back.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import profile.back.domain.entity.board.Together;
-
-public interface TogetherRepository extends JpaRepository<Together,Long>{
-
-}
